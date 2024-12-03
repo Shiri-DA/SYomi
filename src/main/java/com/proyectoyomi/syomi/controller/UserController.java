5dfaced858ec140a9409bba5fc8ed7160e5e075a
Shiri-DA <90511545+Shiri-DA@users.noreply.github.com>
2024-12-03 18:13:10 +0900

    Create Cors Configuration

--- src/main/java/com/proyectoyomi/syomi/controller/UserController.java
+++ src/main/java/com/proyectoyomi/syomi/controller/UserController.java
@@ -2,7 +2,9 @@ package com.proyectoyomi.syomi.controller;
 
 import com.proyectoyomi.syomi.entity.AppUser;
 import com.proyectoyomi.syomi.service.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // Just for config TODO: delete before production
+    @PostConstruct
+    public void initRolesAndUsers() {
+        userService.initRolesAndUsers();
+    }
+
     @PostMapping({"/createUser"})
     public AppUser createUser(@RequestBody AppUser appUser) {
         return userService.createUser(appUser);
     }
+
 }
--- src/main/java/com/proyectoyomi/syomi/service/UserService.java
+++ src/main/java/com/proyectoyomi/syomi/service/UserService.java
@@ -1,17 +1,53 @@
 package com.proyectoyomi.syomi.service;
 
 import com.proyectoyomi.syomi.dao.AppUserDao;
+import com.proyectoyomi.syomi.dao.RoleDao;
 import com.proyectoyomi.syomi.entity.AppUser;
+import com.proyectoyomi.syomi.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     @Autowired
     private AppUserDao appUserDao;
+    @Autowired
+    private RoleDao roleDao;
 
     public AppUser createUser(AppUser user) {
         return appUserDao.save(user);
+    }
+
+    // Just for config TODO: delete before production
+    public void initRolesAndUsers() {
+        Role adminRole = new Role();
+        adminRole.setRoleName("ADMIN");
+        adminRole.setRoleDescription("Administrador");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("USER");
+        userRole.setRoleDescription("Usuario");
+        roleDao.save(userRole);
+
+        AppUser adminUser = new AppUser();
+        adminUser.setUsername("admin");
+        adminUser.setPassword("admin");
+        Set<Role> adminRoles = new HashSet<Role>();
+        adminRoles.add(adminRole);
+        adminUser.setRoles(adminRoles);
+        appUserDao.save(adminUser);
+
+        AppUser user1 = new AppUser();
+        user1.setUsername("user1");
+        user1.setPassword("user1");
+        Set<Role> userRoles = new HashSet<Role>();
+        userRoles.add(userRole);
+        user1.setRoles(userRoles);
+        appUserDao.save(user1);
     }
 }
