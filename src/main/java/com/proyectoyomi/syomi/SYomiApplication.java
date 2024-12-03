1de74e2ae8e5f0234f4a067277026970aada6725
Shiri-DA <90511545+Shiri-DA@users.noreply.github.com>
2024-12-03 14:27:10 +0900

    Basic setup

---
+++ src/main/java/com/proyectoyomi/syomi/SYomiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoyomi.syomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SYomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SYomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyectoyomi/syomi/SYomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoyomi.syomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SYomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
